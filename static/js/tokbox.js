ef2577dfc842a2b0abf57adddd9a19ec93868488
John McLear <john@mclear.co.uk>
2012-12-03 02:08:22 +0000

    easy first size issue fix

--- static/js/tokbox.js
+++ static/js/tokbox.js
@@ -1,7 +1,7 @@
 var $ = require('ep_etherpad-lite/static/js/rjquery').$; // use jQuery
 
 exports.postAceInit = function (hook_name, args, cb) {
-  $('#editorcontainer').css({"right":"357px"});
+  $('#editorcontainer').css({"right":"357px","width":"auto"});
   $('#chatbox').prepend('<div id=tokbox><iframe id="videoEmbed" src="http://api.opentok.com/hl/embed/1emb9ab32ae2766a68996b7afff7fe4965672f5b" width="350" height="265" style="border:none" frameborder="0"></iframe></div>');
   $('#tokbox').css({"z-index":"999999", "position":"absolute", "top":"0px"});
   $('#chatbox').attr('style', 'width: 350px !important');
