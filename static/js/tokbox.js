bd3b736236cb8f48922aa9bb56e1e332ff0d466c
John McLear <john@mclear.co.uk>
2012-12-03 16:18:50 +0000

    Make tokBox execute on demand and make it easier and more obvious how to use it also introduce icon

--- static/js/tokbox.js
+++ static/js/tokbox.js
@@ -1,13 +1,55 @@
-var $ = require('ep_etherpad-lite/static/js/rjquery').$; // use jQuery
-
-exports.postAceInit = function (hook_name, args, cb) {
-  $('#editorcontainer').css({"right":"357px","width":"auto"});
-  $('#chatbox').prepend('<div id=tokbox><iframe id="videoEmbed" src="http://api.opentok.com/hl/embed/1emb9ab32ae2766a68996b7afff7fe4965672f5b" width="350" height="265" style="border:none" frameborder="0"></iframe></div>');
-  $('#tokbox').css({"z-index":"999999", "position":"absolute", "top":"0px"});
-  $('#chatbox').attr('style', 'width: 350px !important');
-  $('#chatbox').addClass("stickyChat");
-  $('#chatbox').show();
-  $('.authorColors').attr('style', 'top: 270px !important');
-  var optPar = $('#options-stickychat').parent();
-  optPar.hide();
+var postAceInit = function(hook, context){
+  if(tokBox && tokBox.key){ // Setup testing else poop out
+    if(tokBox.onByDefault === true){
+      enableTokBox(tokBox.key);
+      showTokBox();
+    }
+  }
+  else{
+    alert('Tokbox key not set...  Set the key in your settings.json file');
+  }
 }
+
+function enableTokBox(key){
+  $("#chatbox").prepend("<div id=tokbox><iframe id='videoEmbed' src='http://api.opentok.com/hl/embed/"+key+"' width='350' height='265' style='border:none' frameborder='0'></iframe></div>");
+}
+
+function showTokBox(){
+  enableTokBox(tokBox.key);
+  $("#editorcontainer").css({"right":"357px","width":"auto"});
+  $("#tokbox").css({"z-index":"999999", "position":"absolute", "top":"0px"});
+  $("#chatbox").attr("style", "width: 350px !important");
+  $("#chatbox").addClass("stickyChat");
+  $("#chatbox").show();
+  $(".authorColors").attr("style", "top: 270px !important");
+  var optPar = $("#options-stickychat").parent();
+  optPar.hide();
+  $("#videoEmbed").show();
+}
+
+function hideTokBox(){
+  $("#editorcontainer").css({"right":"0px","width":"auto"});
+  $("#videoEmbed").remove();
+  $(".authorColors").attr("style", "top: 270px !important");
+  $("#chatbox").attr("style", "width: 200px !important");
+  $("#chatbox").removeClass("stickyChat");
+  $("#chatbox").hide();
+
+}
+
+function toggleTokBox(){
+  if($("#videoEmbed").is(":visible")){
+    hideTokBox();
+  }else{
+    showTokBox();
+  }
+}
+
+$(".tokboxButton").click(function(){ /* On click listener for tokbox button */
+  toggleTokBox();
+});
+
+exports.postAceInit = postAceInit;
+exports.enableTokBox = enableTokBox;
+exports.showTokBox = showTokBox;
+exports.hideTokBox = hideTokBox;
