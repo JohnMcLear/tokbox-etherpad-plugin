7c67cbe922bf477a4d76075c6920fe292a5072a9
John McLear <john@mclear.co.uk>
2014-09-24 10:16:46 +0100

    working sessions, not sure if I should be broadcasting same token to everyone tho..

--- tokbox.js
+++ tokbox.js
@@ -2,31 +2,50 @@ var eejs = require('ep_etherpad-lite/node/eejs/'),
 settings = require('ep_etherpad-lite/node/utils/Settings'),
  OpenTok = require('opentok');
 
+var tokBoxKVS = {};
+
 exports.eejsBlock_scripts = function (hook_name, args, cb) {
   args.content += "<script src='//static.opentok.com/webrtc/v2.2/js/opentok.min.js'></script>";
   return cb();
 }
 
 exports.clientVars = function(hook, context, callback){
-  if(settings.ep_tokbox && settings.ep_tokbox.key && settings.ep_tokbox.secret){ // Setup testing else poop out
 
-    console.warn("Creating tokbox", settings.ep_tokbox);
-    opentok = new OpenTok(settings.ep_tokbox.key, settings.ep_tokbox.secret);
-    opentok.createSession(function(err, session) {
-      if (err) throw err;
-      console.warn("Congrats, Opentok ready to rock and roll with Session ID", session.sessionId);
-
-      var tokBox = {};
-      tokBox.key = settings.ep_tokbox.key || false;
-      tokBox.token = opentok.generateToken(session.sessionId);
-      tokBox.sessionId = session.sessionId;
-      tokBox.onByDefault = settings.ep_tokbox.onByDefault || false;
-      return callback({ep_tokbox: tokBox});
-    });
+  // Make sure settings are setup
+  if(settings.ep_tokbox && settings.ep_tokbox.key && settings.ep_tokbox.secret){
+    // Settings all appear to be in place.  Continue :)
   }else{
     console.error("ep_tokbox.key or secreto not set, set it in settings.json");
     return callback(null);
   }
+
+  // Setup tokbox object we will pass to client
+  var tokBox = {};
+  tokBox.key = settings.ep_tokbox.key || false;
+  tokBox.onByDefault = settings.ep_tokbox.onByDefault || false;
+
+  var padId = context.clientVars.padId;
+
+  // Does session already exist for room?
+  if(tokBoxKVS[padId]){
+    tokBox.sessionId = tokBoxKVS[padId].sessionId;
+    tokBox.token = tokBoxKVS[padId].token;
+    console.warn(tokBox);
+    return callback({ep_tokbox: tokBox});
+  }
+
+  // Session does not exist, creating one.
+  var opentok = new OpenTok(settings.ep_tokbox.key, settings.ep_tokbox.secret);
+  opentok.createSession(function(err, session) {
+    if (err) throw err;
+    tokBox.token = opentok.generateToken(session.sessionId);
+    // We should map sessionId to padId
+    tokBox.sessionId = session.sessionId;
+    tokBoxKVS[padId] = {};
+    tokBoxKVS[padId].token = tokBox.token; // Store to KVS
+    tokBoxKVS[padId].sessionId = session.sessionId; // Store to KVS
+    return callback({ep_tokbox: tokBox});
+  });
 }
 
 exports.eejsBlock_editbarMenuRight = function (hook_name, args, cb){
